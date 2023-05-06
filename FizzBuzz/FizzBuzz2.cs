80a3d3e0e17d68aa87e4141a675ef441e9cd3fa4
developer <ddd98754@gmail.com>
2023-05-06 16:48:17 +0900

    条件ごとに文字列を追加していく方式

---
+++ FizzBuzz/FizzBuzz2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzzGousei
+    {
+        public string convert(int n)
+        {
+            var converted = "";
+            if (n % 3 == 0)
+            {
+                converted += "Fizz";
+            }
+
+            if (n % 5 == 0)
+            {
+                converted += "Buzz";
+            }
+
+            if (converted.Length == 0)
+            {
+                converted = n.ToString();
+            }
+
+            return converted;
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,8 +1,11 @@
 ﻿using FizzBuzz;
-var fizzbuzz = new FizzBuzzStandard();
+
+//var fizzbuzz = new FizzBuzzStandard();
+var fizzbuzz = new FizzBuzzGousei();
 
 for (int i = 1;i <= 30; i++)
 {
     var converted = fizzbuzz.convert(i);
     Console.WriteLine(converted);
 }
+
